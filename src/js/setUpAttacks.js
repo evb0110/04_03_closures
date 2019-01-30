ea71b95fc12a63b02d2655fe662e5a2e882e1458
evb0110 <7149553@gmail.com>
2019-01-31 02:25:44 +0300

    a non-mutating solution

---
+++ src/js/setUpAttacks.js
@@ -0,0 +1,44 @@
+function setUpAttacks(items, shield = true) {
+  const result = [];
+  const aliveIndices = alive(items);
+  const aliveNumber = aliveIndices.length;
+
+  for (let i = 0; i < items.length; i++) {
+    const attack = (loss) => {
+      const lossShare = Math.floor(loss / aliveNumber);
+      const lossRemainder = loss % aliveNumber;
+      const modifiedItems = items.reduce((acc, currentItem, idx) => {
+        const resultantItem = {...currentItem};
+        if (aliveIndices.includes(idx)) {
+          const newHealth = currentItem.health - lossShare - (idx === i ? lossRemainder : 0);
+          const resultantHealth = newHealth > 0 ? newHealth : 0;
+          resultantItem.health = resultantHealth;
+        }
+        return [...acc, resultantItem];
+      }, []);
+      return modifiedItems;
+    }
+    result[i] = attack;
+  }
+  return result;
+}
+
+function alive(items) {
+  // Returns the indices of all the characters that are alive
+  return items.reduce((acc, item, i) => {
+    if (item.health > 0) return [...acc, i];
+    return acc;
+  }, []);
+}
+
+let characters = [
+  {name: 'маг', health: 100},
+  {name: 'лучник', health: 80},
+  {name: 'мечник', health: 10},
+]
+
+const attacks = setUpAttacks(characters);
+
+characters = attacks[1](200); // атакуем лучника 9 баллами урона
+
+console.log(characters)
